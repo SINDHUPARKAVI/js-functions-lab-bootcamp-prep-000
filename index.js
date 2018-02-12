ceaf25b18fd6521e29a77604a85c02ae69f557b1
sindhuparkavi <sindhuparkavi88@gmail.com>
2018-02-12 08:46:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ function happyHolidayTo(holiday,name)
 {
 return `Happy ${holiday}, ${name}!`
 }
+function holidayCountdown(days,holiday)
+{
+return `It's ${days} days until ${holiday}!`
+}
+
 
 
 
